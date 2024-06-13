a1dd2b0b4a7b0a2eaea9616180c3f742a619c8ad
RickyRichy <doublershngh@gmail.com>
2024-06-13 11:34:30 +0700

    fix UI

--- src/components/Home/MainSection/index.js
+++ src/components/Home/MainSection/index.js
@@ -14,7 +14,7 @@ function MainSection() {
         <img className={`${styles.image} ${styles.thumbnail}`} src={"https://media.gody.vn//images/ho-chi-minh/nha-hat-thanh-pho/10-2016/20161021024541-nha-hat-tp-gody%20(5).jpg"} alt="Mô hình nhà hát lớn thành phố Hồ Chí Minh" />
         <img className={`${styles.image} ${styles.thumbnail}`} src={"https://media.gody.vn//images/ho-chi-minh/nha-hat-thanh-pho/10-2016/20161021024545-nha-hat-tp-gody%20(8).jpg"} alt="Mô hình nhà hát lớn thành phố Hồ Chí Minh" />
         <img className={`${styles.image} ${styles.thumbnail}`} src={"https://images.squarespace-cdn.com/content/v1/5930dc9237c5817c00b10842/1602831320943-73MWS5A9ZV9L95XY6HAU/c7992afc4dbaa4e4fdab.jpg?format=1500w"} alt="Mô hình nhà hát lớn thành phố Hồ Chí Minh" />
-        <img className={`${styles.image} ${styles.thumbnail}`} src={"https://gody.vn/public/Media_User/yoshi_mori445316/12-2018/nha-hat-thanh-pho/130703338-20181206111510-nha-hat-thanh-pho.jpg"} alt="Mô hình nhà hát lớn thành phố Hồ Chí Minh" />
+        {/* <img className={`${styles.image} ${styles.thumbnail}`} src={"https://gody.vn/public/Media_User/yoshi_mori445316/12-2018/nha-hat-thanh-pho/130703338-20181206111510-nha-hat-thanh-pho.jpg"} alt="Mô hình nhà hát lớn thành phố Hồ Chí Minh" /> */}
       </div>
       <div className={styles.mapShow}>
         <h2 className={styles.title}>Vị trí của Nhà hát lớn TP.HCM</h2>
